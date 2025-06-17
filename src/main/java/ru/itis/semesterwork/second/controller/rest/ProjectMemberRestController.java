ebb869fae32ef52da13679a34cfe84a7422f3280
Nikita Patshin <cacadada26@yandex.ru>
2025-06-18 02:07:17 +0300

    Added preAuthorize and changed ui.

--- src/main/java/ru/itis/semesterwork/second/controller/rest/ProjectMemberRestController.java
+++ src/main/java/ru/itis/semesterwork/second/controller/rest/ProjectMemberRestController.java
@@ -1,6 +1,7 @@
 package ru.itis.semesterwork.second.controller.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itis.semesterwork.second.api.rest.ProjectMemberRestAPI;
 import ru.itis.semesterwork.second.dto.request.projectmember.AddMemberRequest;
@@ -18,26 +19,31 @@ public class ProjectMemberRestController implements ProjectMemberRestAPI {
     private final ProjectMemberService projectMemberService;
 
     @Override
+    @PreAuthorize("@projectSecurityService.isOwner(#projectId)")
     public void addMember(UUID projectId, AddMemberRequest request) {
         projectMemberService.addMember(projectId, request);
     }
 
     @Override
+    @PreAuthorize("@projectSecurityService.isOwner(#projectId)")
     public void updateMemberRole(UUID projectId, String username, UpdateMemberRoleRequest request) {
         projectMemberService.updateMemberRole(projectId, username, request);
     }
 
     @Override
+    @PreAuthorize("@projectSecurityService.isOwner(#projectId)")
     public void removeMember(UUID projectId, String username) {
         projectMemberService.deleteMember(projectId, username);
     }
 
     @Override
+    @PreAuthorize("@projectSecurityService.isMember(#projectId)")
     public List<ProjectMemberResponse> getProjectMembers(UUID projectId) {
         return projectMemberService.getAllByProjectId(projectId);
     }
 
     @Override
+    @PreAuthorize("@projectSecurityService.isMember(#projectId)")
     public void exitProject(UUID projectId) {
         projectMemberService.exitProject(projectId);
     }
